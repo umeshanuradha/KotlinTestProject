bccc709cea810cd8b439e86270fc95e6409f8bc4
umeshw <umeshwalagedara@gmail.com>
2019-02-18 13:13:39 +0530

    changed added

--- src/app.kt
+++ src/app.kt
@@ -1,4 +1,36 @@
 fun main(args: Array<String>) {
-    println("Hello world 123");
+    println("Hello world 123")
+    println(sum(5,6))
+    println(minus(7,5))
+    printSum(1,3)
+    println(a)
+
+    b += 1;
+    println(b)
+
+    var c : Int;
+    c = 10
+    c += 5;
+    println(c)
 }
+
+fun sum(a: Int, b : Int): Int {
+    return a + b
+}
+
+fun minus(a : Int, b : Int) = a-b
+
+fun printSum(a: Int, b : Int){
+    println("Sum of $a and $b is ${a + b}")
+}
+
+var a : Int = 10
+var b = 2
+
+
+
+
+
+
+
 
